18623e970824467fdb819fc04576332c3d84262c
Killian KOPP <killiankopp@gmail.com>
2025-06-25 09:42:38 +0200

    readme

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# MongoDB déploiement
+# MongoDB
 
-Il faut exécuter install.sh pour déployer une base de données MongoDB.
+## values.yaml
 
-```sh
-install.sh
+```yaml
+persistence:
+  enabled: true
+  size: 1Gi
+  storageClass: "csi-cinder-high-speed"
 ```
+
+Adaptez les valeurs de `size` et `storageClass` selon vos besoins.
+
+## Déploiement de MongoDB
+
+Le déploiement peut être réalisé en utilisant le script `install.sh`.
+
+Ce script va :
+- créer le secret `unikalo-mongodb-secret` contenant les identifiants de connexion. (Pensez à modifier ce nom sur le déploiement Meow si besoin)
+- déployer MongoDB en utilisant ArgoCD
--- install.sh
+++ install.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
 read -p "Entrez le nom de l'application (APP_NAME) : " APP_NAME
-read -p "Entrez le nom du projet ArgoCD (PROJECT_NAME) : " PROJECT_NAME
 read -p "Entrez le nom du secret Kubernetes (SECRET_NAME) : " SECRET_NAME
 read -p "Entrez le namespace Kubernetes (NAMESPACE) : " NAMESPACE
-read -p "Entrez le nom de l'utilisateur MongoDB (USER_NAME) : " USER_NAME
 read -p "Entrez le nom de la base de données MongoDB (DATABASE_NAME) : " DATABASE_NAME
 read -p "Entrez le mot de passe root de la base de données (DATABASE_ROOT_PASS) : " DATABASE_ROOT_PASS
 read -p "Entrez le mot de passe utilisateur de la base de données (DATABASE_USER_PASS) : " DATABASE_USER_PASS
-
-sed -i '' "s/APP_NAME/$APP_NAME/g" app.yaml
-sed -i '' "s/PROJECT_NAME/$PROJECT_NAME/g" app.yaml
-sed -i '' "s/SECRET_NAME/$SECRET_NAME/g" app.yaml
-sed -i '' "s/NAMESPACE/$NAMESPACE/g" app.yaml
-sed -i '' "s/USER_NAME/$USER_NAME/g" app.yaml
-sed -i '' "s/DATABASE_NAME/$DATABASE_NAME/g" app.yaml
 
 kubectl create secret generic "$SECRET_NAME" -n "$NAMESPACE" \
   --from-literal=mongodb-root-password="$DATABASE_ROOT_PASS" \
@@ -24,9 +15,3 @@ kubectl create secret generic "$SECRET_NAME" -n "$NAMESPACE" \
 kubectl apply -f app.yaml -n argocd
 
 echo "Déploiement terminé avec succès."
-
-git add .
-git commit -m "Update app.yaml"
-git push
-
-echo "Le fichier app.yaml a été mis à jour sur Github."
